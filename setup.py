b9d69320e8b0eec9c6da936e943b6caac9831b75
Pragna B <pragnabheemaraju02@gmail.com>
2024-03-04 21:37:15 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+name="Visa-Approval",
+version="0.0.0",
+author="Pragna",
+author_email="pragnabheemaraju02@gmail.com",
+packages=find_packages()
+)
